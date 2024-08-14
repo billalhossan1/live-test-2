80edd6059127e95ebc48d71280c7e72c0320e715
Billal Hossan <bh302333@gmail.com>
2024-08-14 23:23:16 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,48 @@ void main()
 
 class MyApp extends StatelessWidget {  @override
   Widget build(BuildContext context) {
-    return(MaterialApp());
+    return(MaterialApp(
+      home: HomeScreen(),
+    )
+    );
+  }
+}
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+     appBar: AppBar(
+       title: Text("Text Styling App"),
+     ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Flutter Text Styling",style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+                ),
+            SizedBox(height: 10,),
+
+            Text(
+              'Experiment with text styles',
+              style: TextStyle(
+                fontSize: 16,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+
+            SizedBox(height: 10,),
+
+            TextButton(onPressed: (){}, child: Text("Click Me")),
+            SizedBox(height: 10,),
+            Text("Welcome To Flutter!"),
+          ],
+        ),
+      ),
+    );
   }
 }

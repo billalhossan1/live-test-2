ce2c8724f8dedb8c78d16a883d969ca145a78d18
Billal Hossan <bh302333@gmail.com>
2024-08-14 20:31:37 +0600

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
 void main()
-{}
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}

ce5fe1dbe3a63b17d51286a09b7f99ea2f18728f
Billal Hossan <bh302333@gmail.com>
2024-08-14 23:52:44 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'homeScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,70 +14,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Text Styling App"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "Flutter Text Styling",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Experiment with text styles',
-              style: TextStyle(
-                fontSize: 16,
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextButton(
-                onPressed: () {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text("You clicked the button!")));
-                },
-                child: Text("Click Me",
-                ),),
-            SizedBox(
-              height: 10,
-            ),
 
-            Text.rich(TextSpan(
-              text: "Welcome to ",
-              style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                fontSize: 16
-              ),
-              children: <TextSpan>[
-                TextSpan(
-                  text: 'Flutter!',
-                  style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                    // Blue color for "Flutter"
-                      ),
-                ),
-              ],
-            ))
-          ],
-        ),
-      ),
-    );
-  }
-}

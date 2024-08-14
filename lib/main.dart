2dc3d7e7df838b719ad4755467a4006fe72cd6cf
Billal Hossan <bh302333@gmail.com>
2024-08-14 23:40:41 +0600

    update style

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,41 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main()
-{
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {  @override
+class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return(MaterialApp(
+    return (MaterialApp(
       home: HomeScreen(),
-    )
-    );
+    ));
   }
 }
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-     appBar: AppBar(
-       title: Text("Text Styling App"),
-     ),
+      appBar: AppBar(
+        title: Text("Text Styling App"),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text("Flutter Text Styling",style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+            Text(
+              "Flutter Text Styling",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-                ),
-            SizedBox(height: 10,),
-
+            SizedBox(
+              height: 10,
+            ),
             Text(
               'Experiment with text styles',
               style: TextStyle(
@@ -41,12 +43,35 @@ class HomeScreen extends StatelessWidget {
                 fontStyle: FontStyle.italic,
               ),
             ),
+            SizedBox(
+              height: 10,
+            ),
+            TextButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("You clicked the button!")));
+                },
+                child: Text("Click Me",
+                ),),
 
-            SizedBox(height: 10,),
-
-            TextButton(onPressed: (){}, child: Text("Click Me")),
-            SizedBox(height: 10,),
-            Text("Welcome To Flutter!"),
+            Text.rich(TextSpan(
+              text: "Welcome to ",
+              style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                fontSize: 16
+              ),
+              children: <TextSpan>[
+                TextSpan(
+                  text: 'Flutter!',
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                    // Blue color for "Flutter"
+                      ),
+                ),
+              ],
+            ))
           ],
         ),
       ),
